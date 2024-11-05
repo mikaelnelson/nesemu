16b5b25a34ee7ecd99529ba456e78e0c05ef74d9
mikaelnelson <mikaelnelson@gmail.com>
2024-11-05 14:58:40 -0600

    Add Ram & Rom and add to Memory Map

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include "memorymap.h"
+#include "ram.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto memory_map = std::make_shared<MemoryMap>();
+
+    auto internal_ram = std::make_shared<Ram>(0x0800);
+    
+    // Register Internal Ram
+    memory_map->register_device(internal_ram, 0x0000, internal_ram->size());
+
+    // Register Internal Ram Mirrors
+    memory_map->register_device(internal_ram, 0x0800, internal_ram->size());
+    memory_map->register_device(internal_ram, 0x1000, internal_ram->size());
+    memory_map->register_device(internal_ram, 0x1800, internal_ram->size());
+
     return 0;
 }
