00806f36fc4a16c3ec6736d3d1ecc304158a3e5a
mikaelnelson <mikaelnelson@gmail.com>
2024-11-06 11:55:03 -0600

    Add basic NES Cartridge loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <spdlog/spdlog.h>
+
+#include "cartridge.h"
+#include "cartridgeloader.h"
+#include "memoryinterface.h"
 #include "memorymap.h"
 #include "ram.h"
 
-int main() {
-    auto memory_map = std::make_shared<MemoryMap>();
+int main(int argc, const char *argv[]) {
 
+    if (argc < 2) {
+        spdlog::error("Usage: {} <cartridge file>", argv[0]);
+        return 1;
+    }
+
+    auto memory_map = std::make_shared<MemoryMap>();
     auto internal_ram = std::make_shared<Ram>(0x0800);
-    
+
     // Register Internal Ram
     memory_map->register_device(internal_ram, 0x0000, internal_ram->size());
 
@@ -14,6 +23,14 @@ int main() {
     memory_map->register_device(internal_ram, 0x0800, internal_ram->size());
     memory_map->register_device(internal_ram, 0x1000, internal_ram->size());
     memory_map->register_device(internal_ram, 0x1800, internal_ram->size());
+
+    auto cartridge_loader = std::make_shared<CartridgeLoader>();
+
+    auto cartridge = cartridge_loader->load(argv[1]);
+
+    if (cartridge) {
+        (*cartridge)->load(*memory_map);
+    }
 
     return 0;
 }
